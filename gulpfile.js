6436d4bf719826861476fbd9c707173c3da92dfe
roronoa zoro <tienaobitcoins@gmail.com>
2024-02-15 08:08:49 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,49 @@
 const { src, dest, watch } = require("gulp");
-const sass = require('gulp-sass')(require('sass'));
+const sass = require("gulp-sass")(require("sass"));
 const ejs = require("gulp-ejs");
 const rename = require("gulp-rename");
 const eslint = require("gulp-eslint");
 
 function generateHTML(cb) {
-    src("./views/index.ejs")
-        .pipe(ejs({
-            title: "Hello Semaphore!",
-        }))
-        .pipe(rename({
-            extname: ".html"
-        }))
-        .pipe(dest("public"));
-    cb();
+  src("./views/index.ejs")
+    .pipe(
+      ejs({
+        title: "Hello Semaphore!",
+      })
+    )
+    .pipe(
+      rename({
+        extname: ".html",
+      })
+    )
+    .pipe(dest("public"));
+  cb();
 }
 
 exports.html = generateHTML;
 
 function runLinter(cb) {
-    return src(['**/*.js', '!node_modules/**'])
-        .pipe(eslint())
-        .pipe(eslint.format()) 
-        .pipe(eslint.failAfterError())
-        .on('end', function() {
-            cb();
-        });
+  return src(["**/*.js", "!node_modules/**"])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .on("end", function () {
+      cb();
+    });
 }
 
 exports.lint = runLinter;
 
- async function generateCSS(cb) {
-    src('./sass/style.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(dest('public/stylesheets'));
-    cb();
+async function generateCSS(cb) {
+  src("./sass/style.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("public/stylesheets"));
+  cb();
 }
 
 function copy(cb) {
-    src('routes/*.js')
-        .pipe(dest('copies'));
-    cb();
+  src("routes/*.js").pipe(dest("copies"));
+  cb();
 }
 
 exports.copy = copy;
@@ -50,14 +53,14 @@ exports.css = generateCSS;
 const sync = require("browser-sync").create();
 
 function browserSync(cb) {
-    sync.init({
-        server: {
-            baseDir: "./public"
-        }
-    });
-    watch('views/**.ejs', generateHTML);
-    watch('sass/**.scss', generateCSS);
-    watch("./public/**").on('change', sync.reload);
+  sync.init({
+    server: {
+      baseDir: "./public",
+    },
+  });
+  watch("views/**.ejs", generateHTML);
+  watch("sass/**.scss", generateCSS);
+  watch("./public/**").on("change", sync.reload);
 }
 
 exports.sync = browserSync;
--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -1,69 +1,45 @@
-$(document).ready(function(){
-
-
+$(document).ready(function () {
   // toggle menu sidebar
 
-    $(function(){
-      $("#includedContentHeader").load("header.html"); 
-      $("#includedContentFooter").load("footer.html");
-    });
+  $(function () {
+    $("#includedContentHeader").load("header.html");
+    $("#includedContentFooter").load("footer.html");
+  });
 
-    // slick slide
-    $('.slider-activation').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '<div class="slick-prev"><img src="./images/iconprev.png" alt=""></div>',
-        nextArrow: '<div class="slick-next"><img src="./images/iconnext.png" alt=""></div>',
-        dots: true,
-    });
+  // slick slide
+  $(".slider-activation").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:
+      '<div class="slick-prev"><img src="./images/iconprev.png" alt=""></div>',
+    nextArrow:
+      '<div class="slick-next"><img src="./images/iconnext.png" alt=""></div>',
+    dots: true,
+  });
 
-    $(document).find("input:checked[type='radio']").addClass('bounce');   
-    $("input[type='radio']").click(function() {
-        $(this).prop('checked', false);
-        $(this).toggleClass('bounce');
+  $(document).find("input:checked[type='radio']").addClass("bounce");
+  $("input[type='radio']").click(function () {
+    $(this).prop("checked", false);
+    $(this).toggleClass("bounce");
 
-        if( $(this).hasClass('bounce') ) {
-            $(this).prop('checked', true);
-            $(document).find("input:not(:checked)[type='radio']").removeClass('bounce');
-        }
-    });
+    if ($(this).hasClass("bounce")) {
+      $(this).prop("checked", true);
+      $(document)
+        .find("input:not(:checked)[type='radio']")
+        .removeClass("bounce");
+    }
+  });
 
-    let arrayList = document.querySelectorAll(".tab-type li");
+  let arrayList = document.querySelectorAll(".tab-type li");
 
-    arrayList.forEach((elm, id) => {
-      $('#tab-' + id).click(function() {
-        arrayList.forEach((e, id) => {
-          e.classList.remove('active');
-        });
-        elm.classList.add('active');
-        const element = document.getElementById("list-game-tab" + id);
-          element.scrollIntoView({ behavior: 'smooth'});
+  arrayList.forEach((elm, id) => {
+    $("#tab-" + id).click(function () {
+      arrayList.forEach((e, id) => {
+        e.classList.remove("active");
       });
+      elm.classList.add("active");
+      const element = document.getElementById("list-game-tab" + id);
+      element.scrollIntoView({ behavior: "smooth" });
     });
-
-});
-    
-
-$('form').submit(function (e) {
-  e.preventDefault();
-  const data = {
-    password: $('#password').val(),
-    email: $('#email').val(),
-  };
-
-  $.ajax({
-    type: 'POST',
-    url: 'http://localhost:8080/api/auth/login',
-    data: JSON.stringify(data),
-    contentType: 'application/json',
-  })
-    .done((data) => {
-      localStorage.setItem("TOKEN", data.jwt);
-      localStorage.setItem("USER", $('#email').val());
-      location.reload();
-    })
-    .fail((err) => {
-    })
-    .always(() => {
-    });
+  });
 });
